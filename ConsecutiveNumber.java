91050d018fbb7a34a31ec3b2d0f322232da7df52
Suman Singh <148692093+suman1252@users.noreply.github.com>
2024-09-09 15:51:08 +0530

    Add files via upload

---
+++ ConsecutiveNumber.java
@@ -0,0 +1,43 @@
+/***
+ * Suman Kumari
+ * 09-09-2024
+ * Java program that finds whether a given number can be expressed as the sum of two or
+more consecutive natural numbers. If possible, the program should print all possible
+combinations of consecutive natural numbers that sum up to the given number.
+ */
+
+package Week_2_Assignment_1;
+
+import java.util.Scanner;
+
+public class ConsecutiveNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.print(Constant.ENTER_NUMBER);
+			int n = sc.nextInt();
+			if(n <= 0) {
+	        	System.out.println("Invalid Number");
+	        }
+			else {
+				System.out.println(Constant.CONSECUTIVE_NUMBER + n + ":");
+				ConsecutiveSum(n);
+			}
+		}
+	}
+	public static void ConsecutiveSum(int n) {
+		for(int i = 2; i < n; i++) {
+			int k = (n - (i * (i - 1)) / 2) / i;
+			if (k > 0 && (n - (i * (i-1)) / 2) % i == 0){
+				printConsecutiveNumbers(k,i);
+			}
+		}
+	}
+    public static void printConsecutiveNumbers(int k, int i) {
+        System.out.print(Constant.START_FROM + k + ": ");
+        for (int j = 0; j < i; j++) {
+            System.out.print((k + j) + " ");
+        }
+	     System.out.println();
+    }
+}
---
+++ Constant.java
@@ -0,0 +1,16 @@
+package Week_2_Assignment_1;
+
+public class Constant {
+	
+	public static String ENTER_NUMBER = "Enter a number: "; 
+	public static String ENTER_STRING = "Enter any String: "; 
+	public static String START_FROM = "Starting from: "; 
+	public static String ENTER_PATTERN = "Enter the shift pattern (comma-separated): "; 
+	public static String ENCRYPTED_STRING = "Encrypted String: ";
+	public static String SERIES_OF_INTEGERS = "Enter the series of number: ";
+	
+	
+	public static String SUM_OF_DIGIT = "The sum of digits until it becomes a single digit is: ";
+	public static String CONSECUTIVE_NUMBER = "Possible combinations of consecutive natural numbers that sum to "; 
+	
+}
